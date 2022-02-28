6bcb5ac7eb5178d5a66dc686ef2e6c98567d7871
LDS <39432361+Ldscrawler2018@users.noreply.github.com>
2022-02-28 18:27:23 -0500

    prediction code----get the probability of rain in future 50 days (without evidence)

--- Assignment 2/Prediction.py
+++ Assignment 2/Prediction.py
@@ -1,11 +1,40 @@
+
 def prediction(evidence_data_add, prior, start_day, end_day):
     # you need to implement this method.
 
     x_prob_rain = []
+    x_prob_rain_pre = []
     # x_prob_sunny[i] = 1 - x_prob_rain[i]
+    umbrella ={1:(0.9,0.2),0:(0.1,0.8)}   #key: take umbrella or not, value: rain or not
+    transition ={1:0.7,0:0.3}             #key: yesterday rain or not, value: the probability of rain in the next day
 
+    # filtering
+    with open(evidence_data_add) as f:
+        cur_data = f.readline()
+
+        while cur_data:
+            evidence = cur_data.strip().split("\t")[1]   # obtain evidence
+            evidence = 0 if evidence=='no umbrella' else 1
+            previous_date_r = x_prob_rain_pre[-1] if x_prob_rain_pre else prior[0]   # obtain previous probability distribution
+
+            T_r = previous_date_r*transition[1] + (1-previous_date_r)*transition[0]
+            T_s = 1-T_r
+
+            cur_e_r = umbrella[evidence][0]*T_r
+            cur_e_s = umbrella[evidence][1]*T_s
+            s = cur_e_r+cur_e_s
+            cur_e_r = cur_e_r/s
+            x_prob_rain_pre.append(cur_e_r)
+
+            cur_data = f.readline()
+
+    x_prob_rain_pre = x_prob_rain_pre[-1]
+
+    # make prediction
     for i in range(start_day, end_day+1):
-        x_prob_rain.append(0.0)
+        previous_date_r = x_prob_rain[-1] if x_prob_rain else x_prob_rain_pre
+        T_r = previous_date_r * transition[1] + (1 - previous_date_r) * transition[0]
+        x_prob_rain.append(T_r)
 
     return x_prob_rain
 
